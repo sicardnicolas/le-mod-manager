fcce51c7d82582fe99776cd1252cfd10d4d58da7
Nicolas Sicard <nicolas@sicard.me>
2021-11-03 23:29:35 +0100

    feat: init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mod Manager
+
+This application aims to ease the mod management for your games.
+
+## Technical stack
+
+- [Electron](https://www.electronjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## Vision
+
+Provide a multiplatform app to handle mods for your games.
+
+## TODO
+
+First goal: Handle mods deployment for 
+[The Elder Scrolls V: Skyrim](https://elderscrolls.bethesda.net/fr/skyrim) 
+(legacy edition) running on Proton on Linux.
+
+Steps:
+- mock basic interface
+- integrate interface
+- capture vortex mod manager links from nexus mods
+- download a mod
+- deploy a mod to Skyrim's directory
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("template/index.html");
+}
+
+// Windaube & Linux
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Darwin
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
